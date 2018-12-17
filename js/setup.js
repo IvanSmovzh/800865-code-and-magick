ba5b1be4a57d1a180c3757b6460264d7d4fe1100
IvanSmovzh <faceoffaceless@gmail.com>
2018-12-17 15:27:52 +0500

    Почти готово изменение цвета

--- js/setup.js
+++ js/setup.js
@@ -96,4 +96,107 @@ var startUp = function () {
   renderWizards();
 };
 startUp();
+/**
+ * если фокус на имени ввода то окно не закрывается
+ * нажатие на сохранить отправляет форму так же через enter
+ */
+var setup = document.querySelector('.setup');
+var setupOpenButton = document.querySelector('.setup-open');
+var setupCloseButton = document.querySelector('.setup-close');
+var setupUserName = document.querySelector('.setup-user-name');
+var setupOpenIcon = setupOpenButton.querySelector('.setup-open-icon');
+var ENTER = 13;
+var ESC = 27;
 
+var toggleClassList = function () {
+  setup.classList.toggle('hidden');
+};
+
+setupCloseButton.addEventListener('click', toggleClassList);
+setupCloseButton.setAttribute('tabindex', '0');
+setupOpenButton.addEventListener('click', toggleClassList);
+setupOpenIcon.setAttribute('tabindex', '0');
+
+
+setupOpenIcon.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER) {
+    setup.classList.remove('hidden');
+  }
+});
+
+setupCloseButton.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER) {
+    setup.classList.add('hidden');
+  }
+});
+
+// setupUserName.addEventListener('keydown', function (evt) {
+//   if (setupUserName.activeElement) {
+//     return evt;
+//   } else {
+//     if (evt.keyCode === ESC) {
+//       setup.classList.add('hidden');
+//     }
+//   }
+// });
+
+
+// ИЗМЕНЕНИЕ ВНЕШНЕГО ВИДА ВОЛШЕБНИКА
+
+var mantieWizardColor = document.querySelector('.wizard-coat');
+var eyesWizardColor = document.querySelector('.wizard-eyes');
+var fireballWizardColor = document.querySelector('.setup-fireball-wrap');
+
+// Возможные цвета мантии
+var colorMantie = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+// Возможные цвета глаз
+var colorEyes = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
+// Возможные цвета фаербола
+var colorFireball = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
+/**
+ * Изменение цвета мантии по клику
+ */
+var setupWizardWrap = document.querySelector('.setup-wizard-wrap');
+var mantieColor = '';
+mantieWizardColor.addEventListener('click', function () {
+  document.querySelector('.wizard-coat').style.fill = colorMantie[getRandInteger(0, colorMantie.length)];
+}
+);
+// setupWizardWrap.querySelector('input[name="coat-color"]').value = mantieColor;
+
+
+/**
+ * Изменение цвета глаз по клику
+ */
+eyesWizardColor.addEventListener('click', function () {
+  document.querySelector('.wizard-eyes').style.fill = colorEyes[getRandInteger(0, colorEyes.length)];
+});
+
+/**
+ * Изменение цвета фаербола по клику
+ */
+fireballWizardColor.addEventListener('click', function () {
+  document.querySelector('.setup-fireball-wrap').style.background = colorFireball[getRandInteger(0, colorFireball.length)];
+});
